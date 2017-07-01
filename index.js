28eeea7434e4836f2bd1d1c2a5834bee9cf15ab1
Ubuntu <ubuntu@ip-172-31-43-86.us-east-2.compute.internal>
2017-07-01 13:23:48 +0000

    mapdemo

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const PORT_NUMBER = 8080
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const path = require("path")
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/templates/mymap.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+//app.get('/', function (req, res) {
+  //res.send('Hello World!')
+//})
 
 app.listen(PORT_NUMBER, function () {
   console.log('Example app listening on' +  PORT_NUMBER + ' !')
