062449c64df8c88eabe8bd2143b3ff84d9bff2df
Ubuntu <ubuntu@ip-172-31-43-86.us-east-2.compute.internal>
2017-07-01 19:33:05 +0000

    fullscreen&jquery&underscore

--- js/mymap.js
+++ js/mymap.js
@@ -1,6 +1,6 @@
 function initMap() {
     var uluru = {lat: 40.766, lng: -73.946};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map($('#map')[0], {
         zoom: 4,
         center: uluru
     });
