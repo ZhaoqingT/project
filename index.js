d7bd70d9964d08a87b752618796e04e8f364f6c7
Ubuntu <ubuntu@ip-172-31-43-86.us-east-2.compute.internal>
2017-07-02 22:49:58 +0000

    zoomin&search

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express')
-const app = express()
-const PORT_NUMBER = 8080
 const path = require("path")
+const request = require('request');
+const app = express()
+
+const PORT_NUMBER = 8080;
+const GOOGLE_KEY = 'AIzaSyDHNnsp8pXJTrCtHF1sizDksBFAyRq_hVo';
+const NEARBY_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/templates/mymap.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
-//app.get('/', function (req, res) {
-  //res.send('Hello World!')
-//})
+app.get('/nearby_search', function(req, api_res){
+    var radius = req.query.radius ? req.query.radius : 150;
+    var params = {
+	'key':GOOGLE_KEY,
+	'location':req.query.location,
+	'radius':radius,
+	'type':req.query.type
+    };
+    request({url:NEARBY_SEARCH_URL, qs:params}, function(err, res, body){
+        if(res.statusCode == 200){
+            api_res.json(body);
+        }else {
+        }
+    });
+});
 app.use('/css', express.static('css'))
 app.use('/js',express.static('js'))
 app.listen(PORT_NUMBER, function () {
--- js/mymap.js
+++ js/mymap.js
@@ -1,12 +1,31 @@
+var DEFAULT_ZOOM = 15;
+var GOOGLE_API_KEY = 'AIzaSyDHNnsp8pXJTrCtHF1sizDksBFAyRq_hVo';
+
+
 function initMap() {
-    var uluru = {lat: 40.766, lng: -73.946};
+    var position = {lat: 40.766, lng: -73.946};
     var map = new google.maps.Map($('#map')[0], {
-        zoom: 4,
-        center: uluru
+        zoom: DEFAULT_ZOOM,
+        center: position
     });
     var marker = new google.maps.Marker({
-        position: uluru,
+        position: position,
         map: map
+    });
+    $.ajax({
+	url:'/nearby_search',
+	data:{
+	    'location':position.lag + ',' + position.lng,
+	    'type':'restaurant',
+	    'key':GOOGLE_API_KEY,
+ 	    'radius':500
+	},
+	success:function(data){
+	    debugger;
+  	},
+	failure:function(data){
+	    debugger;
+	}
     });
 }
 
