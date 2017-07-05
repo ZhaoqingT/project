fb4e8111c78b9518fe632c6e7dd6ac92b73669f8
Ubuntu <ubuntu@ip-172-31-43-86.us-east-2.compute.internal>
2017-07-05 21:52:29 +0000

    searchnearbyandsetmarker

--- js/mymap.js
+++ js/mymap.js
@@ -1,31 +1,38 @@
 var DEFAULT_ZOOM = 15;
 var GOOGLE_API_KEY = 'AIzaSyDHNnsp8pXJTrCtHF1sizDksBFAyRq_hVo';
+var DEFAULT_RADIUS = 1500;
+var DEFAULT_LAT = 40.766082;
+var DEFAULT_LNG = -73.946066;
 
-
-function initMap() {
-    var position = {lat: 40.766, lng: -73.946};
-    var map = new google.maps.Map($('#map')[0], {
-        zoom: DEFAULT_ZOOM,
-        center: position
-    });
-    var marker = new google.maps.Marker({
-        position: position,
-        map: map
-    });
-    $.ajax({
-	url:'/nearby_search',
-	data:{
-	    'location':position.lag + ',' + position.lng,
-	    'type':'restaurant',
-	    'key':GOOGLE_API_KEY,
- 	    'radius':500
-	},
-	success:function(data){
-	    debugger;
-  	},
-	failure:function(data){
-	    debugger;
-	}
-    });
-}
+$(function(){
+    function initMap() {
+	var map = new google.maps.Map($('#map')[0], {
+	    zoom:DEFAULT_ZOOM,
+	    center:{
+		lat:DEFAULT_LAT,
+		lng: DEFAULT_LNG
+	    }
+	});
+	var params = {
+	    'location':new google.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+	    'radius':DEFAULT_RADIUS,
+	    'type':'restaurant'
+	};
+	service = new google.maps.places.PlacesService(map);
+	service.nearbySearch(params, function(results, status) {
+	    if(status == google.maps.places.PlacesServiceStatus.OK){
+		_.each(results, function(place){
+		    var marker = new google.maps.Marker({
+			position: {
+			    'lat': place.geometry.location.lat(),
+			    'lng': place.geometry.location.lng()
+			},
+			map: map
+		    });
+		});
+	    }
+	});
+    }
+    initMap();
+});
 
